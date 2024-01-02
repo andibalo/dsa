6ea493d519075f46f05281cd536c240c569cd6d8
Andi Usman Balo <andibalo213@gmail.com>
2024-01-02 19:45:10 +0700

    addditional array and obj question

--- array and objects/maxsubarraysum.js
+++ array and objects/maxsubarraysum.js
@@ -14,7 +14,31 @@
 
 // Space Complexity - O(1)
 
+function maxSubarraySum(arr, num) {
 
-function maxSubarraySum(){
-    // add whatever parameters you deem necessary - good luck!
-  }
+    if (arr.length < num) {
+        console.log(null)
+        return 
+    }
+
+    let maxSum = 0
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+
+    let tempSum = maxSum
+    for(let i = num; i < arr.length; i++){
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(tempSum, maxSum)
+    }
+
+    console.log(maxSum)
+    return
+}
+
+
+maxSubarraySum([100, 200, 300, 400], 2) // 700
+maxSubarraySum([1,4,2,10,23,3,1,0,20], 4)  // 39 
+maxSubarraySum([-3,4,0,-2,6,-1], 2) // 5
+maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2) // 5
+maxSubarraySum([2,3], 3) // null
