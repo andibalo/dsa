84dd4a82522d5a7aef616947e4b7a6ba1cd06d46
Andi Usman Balo <andi.usman@efishery.com>
2024-01-02 19:42:36 +0700

    additional array and object questions

---
+++ array and objects/minSubArrayLen.js
@@ -0,0 +1,56 @@
+// Sliding Window - minSubArrayLen
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+// Examples:
+
+// minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> because [4,3] is the smallest subarray
+// minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
+// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],39) // 3
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],55) // 5
+// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],95) // 0
+// Time Complexity - O(n)
+
+// Space Complexity - O(1)
+
+function minSubArrayLen(arr, num) {
+    
+    let minArrLen = 0
+    let tempArrLen = 0
+    let left = 0
+    let tempSum = 0
+  
+    for (let right = 0; right < arr.length; right++) {
+
+        if (tempArrLen == 0) {
+            tempSum = arr[left]
+        } else {
+            tempSum += arr[right]
+        }
+
+        if(tempSum < num){
+            tempArrLen++
+            continue
+        } 
+
+        if (tempSum >= num) {
+            left = right
+            minArrLen = Math.min(minArrLen, tempArrLen)
+            tempArrLen = 0
+        }
+    }
+
+    console.log(minArrLen)
+    return
+}
+
+
+minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> because [4,3] is the smallest subarray
+// minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
+// minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],39) // 3
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],55) // 5
+// minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11) // 2
+// minSubArrayLen([1,4,16,22,5,7,8,9,10],95) // 0
