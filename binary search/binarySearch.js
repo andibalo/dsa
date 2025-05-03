56aa43e78e631f7f1713944fa2da0ac514572efd
Andi Usman Balo <andibalo213@gmail.com>
2025-05-04 00:11:21 +0700

    704. Binary Search (Leetcode)

--- binary search/binarySearch.js
+++ binary search/binarySearch.js
@@ -1,3 +1,5 @@
+// 704. Binary Search
+// https://leetcode.com/problems/binary-search/description/
 // Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
 
 // This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
@@ -21,32 +23,63 @@
 //   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
 // ], 100) // -1
 
-function binarySearch(arr, num) {
-    // add whatever parameters you deem necessary - good luck!
+// SOLUTION 1
+/*
+1. Create left, middle and right pointer.
+2. Use a while loop using the condition left <= right for search, loop will break if right < left or left > right
+3. Check if middle === target
+4. If target > middle, we can move left pointer by setting left = middle + 1. Adding 1 is important to break out of loop (step 2)
+5. If target < middle, we can move right pointer by setting right = middle - 1. Minus 1 is important to break out of loop (step 2)
+6. Recalculate middle pointer, middle = left + (right - left / 2)
 
-    let left = 0
-    let right = arr.length - 1
+Note: Step 4-6 will handle if there is only three elements left. It will converge into one and be compared to the target value
+*/
 
-    let middle = Math.floor((left + right) / 2)
+var binarySearch = function(nums, target) {
+    let targetIndex = -1
 
-    while (left <= right){
-
-        if(arr[middle] == num){
-            console.log(middle) 
-            return
+    if(nums.length <= 2) {
+        if(nums[0] === target) {
+            targetIndex = 0
+            return targetIndex
+        }
+ 
+        if(nums[1] === target) {
+            targetIndex = 1
+            return targetIndex
         }
 
-        if(arr[middle] < num){
-            left = middle + 1
-            middle = Math.floor((left + right) / 2) 
-        } else {
+        return targetIndex
+    }
+
+    let left = 0 
+    let right = nums.length - 1
+
+    let middle =  Math.floor((right - left) / 2) 
+
+    while(left <= right){
+
+        if(nums[middle] === target){
+            targetIndex = middle
+            break
+        }
+
+        if(target < nums[middle]){
             right = middle - 1
-            middle = Math.floor((left + right) / 2) 
+            middle = left + Math.floor((right - left) / 2) 
+            continue
+        }
+
+        if(target > nums[middle]){
+            left = middle + 1
+            middle = left + Math.floor((right - left) / 2) 
+            continue
         }
     }
 
-    console.log(-1)
-}
+    return targetIndex
+};
+
 
 binarySearch([1,2,3,4,5],-2) // 1
 // binarySearch([1,2,3,4,5],3) // 2
